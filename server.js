b9c355744016fed820a4a7b6bee3ce4a3003458b
Reti Kalde <reti@Retis-MacBook-Air.local>
2023-09-28 21:11:47 +0300

    week 1 code

---
+++ server.js
@@ -0,0 +1,16 @@
+//console.log("Hello, World!");
+
+// ctrl + c - kui terminal on kokku jooksunud; konsoolis toimuvate protsesside peatamine
+
+let firstName = 'Harry';
+let lastName = "Potter";
+//teksilised andmed on ülakomade või jutumärkide vahel
+//console.log(firstName); //konsool näitab andmeid, mis on salvestatud muutujasse FirstName
+
+//console.log("Hello, " + firstName + "!");
+
+//templates literals
+//console.log(`Hello, ${firstName}!`);
+
+let greeting = `Hello, ${firstName} ${lastName}!`;
+console.log(greeting);
